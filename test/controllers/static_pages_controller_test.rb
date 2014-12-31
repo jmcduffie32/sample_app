75d6ef36d950eaf426de7862bfa168504c795f14
Jon McDuffie <jon@jrmcduffie.com>
2014-12-31 14:52:13 -0500

    Eliminate repetition (solves exercise 3.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About#{@title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact#{@title}"
+
 end
