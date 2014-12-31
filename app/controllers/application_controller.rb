f3d9b56f09299a77e22f2b2d1440911af260887b
Jon McDuffie <jon@jrmcduffie.com>
2014-12-31 14:51:02 -0500

    Eliminate repetition (solves exercise 3.1)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  root 'static_pages#home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+
+
 end
